cd8e1a55b3510207bf015fadf15462ee1d0e4cde
Jennifer <j.schneeweiss@aol.com>
2022-09-14 20:39:09 +0200

    feat: Eingabeformular

--- NochmalVonVorne/projektzumreinkommen/src/App.js
+++ NochmalVonVorne/projektzumreinkommen/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//der Import für React
+import React from 'react';
+class App extends React.Component {
+  //der Konstruktor
+  constructor(props) {
+    //den Konstruktor der übergeordneten Klasse aufrufen
+    super(props);
+    //den State setzen
+    //er ist hier leer
+    this.state = { eingabe: '' };
+    //die Methoden binden
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  //die Methoden für die Ereignisbehandlung
+  //bei Veränderungen wird der State über setState() angepasst
+  handleChange(event) {
+    console.log('event', event);
+    this.setState({ eingabe: event.target.value });
+  }
+  //beim Abschicken wird der Wert ausgegeben
+  handleSubmit(event) {
+    alert('Der Wert ' + this.state.eingabe + ' wurde eingegeben.');
+    event.preventDefault();
+  }
+  //die Methode render()
+  render() {
+    return (
+      <div>
+        <h1>Ein einfaches Formular</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Ihre Eingabe?"
+            value={this.state.eingabe}
+            onChange={this.handleChange} />
+          <input type="submit" value="Ausgeben" />
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
