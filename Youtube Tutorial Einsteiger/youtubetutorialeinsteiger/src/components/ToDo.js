34130397cdf14ed6de557a0db113e7cccfcd0eaf
Jennifer <j.schneeweiss@aol.com>
2022-09-18 15:52:56 +0200

    feat: React Tutorial einsteiger

---
+++ Youtube Tutorial Einsteiger/youtubetutorialeinsteiger/src/components/ToDo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ToDo = ({ description, done, onChangeToDo, onDeleteToDo, index }) => {
+
+
+
+    const changeToDo = () => {
+        console.log("hallo");
+    }
+
+    return (
+        <div>
+            <div className={
+                done ? "flex justify-between items-center p-2 bg-green-600 text-white"
+                    : "flex justify-between items-center p-2 bg-red-500 text-white"
+            }
+            >
+                <h1 className='text-lg cursor-pointer'
+                    onClick={() => {
+                        onChangeToDo(index);
+                    }}>
+                    {description}
+                </h1>
+                <button className='text-lg bg-gray-400 p-2 text-white' onClick={() => {onDeleteToDo(index)}}>Löschen</button>
+            </div>
+        </div>
+    )
+}
+
+export default ToDo;
---
+++ Youtube Tutorial Einsteiger/youtubetutorialeinsteiger/src/components/ToDoList.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ToDo from './ToDo';
+import { useState, useEffect } from 'react';
+
+const ToDoList = () => {
+
+    const [openCount, CountOpenToDos] = useState(0);
+    const [toDos, setToDos] = useState(() => {
+        const items = localStorage.getItem("items");
+        const parsed = JSON.parse(items);
+        return parsed || [];
+    });
+    const [textInput, setTextInput] = useState("");
+
+    const changeText = (e) => {
+        setTextInput(e.target.value);        
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        const newToDos = [...toDos, {description : textInput, done : false}];
+        setToDos(newToDos);
+        setTextInput("");
+    };
+
+    const countOpen = () => {
+        const doneToDos = toDos.filter((item) => {
+            return !item.done;
+        });
+        CountOpenToDos(doneToDos.length);
+    };
+
+    const changeToDo = (index) => {
+        const newToDos = [...toDos]; // nie direkt den State ändern. So erhält man eine kopie davon. Der kann wiederum geändert werden
+        if (newToDos[index].done)
+            newToDos[index].done = false;
+        else
+            newToDos[index].done = true;
+
+        setToDos(newToDos);
+    };
+
+    const deleteToDo = (index) => {
+        const newToDos = [...toDos];
+        newToDos.splice(index, 1);
+        setToDos(newToDos);
+    };
+
+    useEffect(() => {
+        countOpen();
+        localStorage.setItem("items", JSON.stringify(toDos));
+    }, [toDos]);
+
+    return (
+        <div className="shadow-sm hover:shadow-lg">
+            <div className="text-center bg-gray-900 text-white py-4 font-semibold">
+                <h1 className='text-3xl'>Unsere ToDo's</h1>
+                <h2>Offene ToDo's: {openCount}</h2>
+                <form className='grid grid-cols-3 py-2'>
+                    <input
+                        onChange={changeText}
+                        type="text"
+                        value={textInput}
+                        placeholder='Neues ToDo...'
+                        className='col-span-2 py-2 text-gray-900'
+                    ></input>
+                    <input
+                    onClick={submit}
+                        type="submit"
+                        value="Add ToDo"
+                        className='col-span-1 text-gray-900 bg-gray-100 cursor-pointer'
+                    ></input>
+                </form>
+            </div>
+
+            {toDos.map((item, index) => {
+                return (
+                    <ToDo
+                        description={item.description}
+                        done={item.done}
+                        key={index}
+                        index={index}
+                        onChangeToDo={changeToDo}
+                        onDeleteToDo={deleteToDo}
+                    ></ToDo>
+                );
+            })}
+        </div>
+    )
+}
+
+export default ToDoList
