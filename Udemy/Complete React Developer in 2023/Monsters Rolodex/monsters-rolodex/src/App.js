acc5bddd0f5fd5c76df16fe348d399548b8c7849
jschneeweiss <j.schneeweiss@aol.com>
2022-11-20 14:32:56 +0100

    feat: Vorstellung

--- Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
+++ Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p>Hallo, mein Name ist Jennifer</p>
         </p>
         <a
           className="App-link"
