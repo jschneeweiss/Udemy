f041e14869da45108737e345486abba340dab5a7
jschneeweiss <j.schneeweiss@aol.com>
2022-11-22 19:08:32 +0100

    feat: meine Variante filter Monster

--- Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
+++ Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
@@ -9,16 +9,22 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      filterMonsters: []
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(users =>
         this.setState(
           () => {
-            return { monsters: users }
+            return { 
+              monsters: users,
+              filterMonsters: users
+             }
           },
           () => {
             console.log(this.state);
@@ -27,11 +33,24 @@ class App extends Component {
       );
   }
 
+  filterName = (event) => {    
+    this.setState({     
+      filterMonsters: this.state.monsters.filter((monster) => monster.name.toLowerCase().indexOf(event.target.value) > -1)
+    })
+  }
+
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={this.filterName}
+        />
         {
-          this.state.monsters.map((monster) => {
+          this.state.filterMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
