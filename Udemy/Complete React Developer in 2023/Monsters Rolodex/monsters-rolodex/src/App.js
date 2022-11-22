c9dcfe03ee9c25583ff5b1f1f2b69c48f8b00f1a
jschneeweiss <j.schneeweiss@aol.com>
2022-11-22 19:30:52 +0100

    feat: Musterlösung

--- Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
+++ Udemy/Complete React Developer in 2023/Monsters Rolodex/monsters-rolodex/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      filterMonsters: []
+      searchField: ''
     };
     console.log('constructor');
   }
@@ -21,36 +21,34 @@ class App extends Component {
       .then(users =>
         this.setState(
           () => {
-            return { 
-              monsters: users,
-              filterMonsters: users
-             }
-          },
-          () => {
-            console.log(this.state);
+            return { monsters: users }
           }
         )
       );
   }
 
-  filterName = (event) => {    
-    this.setState({     
-      filterMonsters: this.state.monsters.filter((monster) => monster.name.toLowerCase().indexOf(event.target.value) > -1)
-    })
-  }
-
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
           className='search-box'
           type='search'
           placeholder='search monsters'
-          onChange={this.filterName}
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
         />
         {
-          this.state.filterMonsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
