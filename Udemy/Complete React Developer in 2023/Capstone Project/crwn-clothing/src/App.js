a93e70441306feddbfcad22978ca0160f3611a11
jschneeweiss <j.schneeweiss@aol.com>
2022-12-02 19:23:17 +0100

    feat: Adding Sass

--- Udemy/Complete React Developer in 2023/Capstone Project/crwn-clothing/src/App.js
+++ Udemy/Complete React Developer in 2023/Capstone Project/crwn-clothing/src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
@@ -20,16 +22,16 @@ const App = () => {
     {
       id: 5,
       title: 'Mens',
-    }
-  ]
+    },
+  ];
 
   return (
     <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='backgoung-image'/>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image'/>
           <div className='category-body-container'>
-            <h1>{title}</h1>
+            <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
