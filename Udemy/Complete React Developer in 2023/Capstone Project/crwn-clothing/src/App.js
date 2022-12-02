e6294247c1a2ea0169a3364417f0c5e0cf9d2a71
jschneeweiss <j.schneeweiss@aol.com>
2022-12-02 18:59:46 +0100

    feat: Capstone Project

---
+++ Udemy/Complete React Developer in 2023/Capstone Project/crwn-clothing/src/App.js
@@ -0,0 +1,43 @@
+const App = () => {
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* {<img />} */}
+        <div className='category-body-container'>
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* {<img />} */}
+        <div className='category-body-container'>
+          <h1>Jackets</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* {<img />} */}
+        <div className='category-body-container'>
+          <h1>Sneakers</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* {<img />} */}
+        <div className='category-body-container'>
+          <h1>Womens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* {<img />} */}
+        <div className='category-body-container'>
+          <h1>Mens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
